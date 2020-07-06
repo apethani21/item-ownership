4110832049cf9c295b8e1148a0833a8266de77b1
Ajay Pethani <ajaypethani97@gmail.com>
2020-07-06 18:45:55 +0100

    general items not just tech items

--- tech_ages.py
+++ tech_ages.py
@@ -7,22 +7,22 @@ from dateutil.utils import today
 from tabulate import tabulate
 
 
-def get_tech_purchase_dates() -> dict:
+def get_purchase_dates() -> dict:
     home = os.path.expanduser("~")
-    with open(f"{home}/tech-purchase-dates.json", "r") as f:
+    with open(f"{home}/purchase-dates.json", "r") as f:
         tech_purchase_dates = json.loads(f.read())
     return tech_purchase_dates
 
 
 def print_ownership_times():
-    tech_purchase_dates = get_tech_purchase_dates()
-    tech_purchase_dates = {tech: parse(purchase_date, dayfirst=True)
-                           for tech, purchase_date in tech_purchase_dates.items()}
-    tech_ages = {tech: relativedelta.relativedelta(today(), purchase_date)
-                 for tech, purchase_date in tech_purchase_dates.items()}
-    tech_ages = {tech: f"{age.years} years, {age.months} months, and {age.days} days"
-                 for tech, age in tech_ages.items()}
-    print(tabulate(tech_ages.items(),
+    purchase_dates = get_purchase_dates()
+    purchase_dates = {item: parse(purchase_date, dayfirst=True)
+                           for item, purchase_date in purchase_dates.items()}
+    ages = {item: relativedelta.relativedelta(today(), purchase_date)
+            for item, purchase_date in purchase_dates.items()}
+    ages = {item: f"{age.years} years, {age.months} months, and {age.days} days"
+            for item, age in ages.items()}
+    print(tabulate(ages.items(),
                    headers=["Item", "Age"],
                    tablefmt="psql"))
     return
